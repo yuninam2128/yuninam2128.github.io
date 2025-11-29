516492489ffff0773e484da511a1a6607dde3560
yuninam2128 <162120020+yuninam2128@users.noreply.github.com>
2025-11-29 12:54:12 +0900

    [fix] top level로 파일 올리기

--- yuni-portfolio/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  // ex base: '/my_project/', 
-  base: "/yuninam2128.github.io/",
-  plugins: [react()],
-});
